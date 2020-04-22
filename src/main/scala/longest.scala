7e0768143e7d2dc37b9e578fc4a8d25bdf2cb019
Incessy <incesu17@itu.edu.tr>
2020-04-22 21:24:30 +0300

    Works correctly.

--- src/main/scala/longest.scala
+++ src/main/scala/longest.scala
@@ -1,8 +1,9 @@
 object longest extends App{
+
   println("enter your text")
   val sentence = readLine()
 
-  implicit def string2ordering(s: String) = s.length
+  implicit def longest(s: String) = s.length
   val words = (sentence.split(" +"))
   println(
     words.maxBy(_.length)
